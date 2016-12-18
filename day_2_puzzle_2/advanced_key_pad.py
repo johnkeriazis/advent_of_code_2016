886d480029861c976dadc904eac259c3901e30d3
John Keriazis <jkeriazis@hedgeserv.com>
2016-12-18 17:52:23 -0500

    advanced keypad first cut

--- day_2_puzzle_2/advanced_key_pad.py
+++ day_2_puzzle_2/advanced_key_pad.py
@@ -7,10 +7,23 @@ key_pad = [
 ]
 
 
+def is_valid_cell(row, col):
+    return key_pad[row][col] is not None
+
 def up(row, col):
-    if row > 0 and key_pad[row -1][col] is not None:
+    if row > 0 and is_valid_cell(row -1, col):
         row -= 1
     return row
+
+def down(row, col):
+    if row < 4 and is_valid_cell(row + 1, col):
+        row += 1
+    return row
+
+def left(row, col):
+    if col > 0 and is_valid_cell(row, col -1):
+        col -= 1
+    return col
 
 
 def advanced_security_code_generator(input_rows):
@@ -22,12 +35,22 @@ def advanced_security_code_generator(input_rows):
         for move in input_row:
             if move == 'U':
                 row = up(row, col)
-            elif move == 'D' and row < 2:
-                row += 1
-            elif move == 'L' and col > 0:
-                col -= 1
-            elif move == 'R' and col < 2:
-                col += 1
+            elif move == 'D':
+                row = down(row, col)
+            elif move == 'L':
+                col = left(row, col)
+            elif move == 'R':
+                col = right(row, col)
         result += key_pad[row][col]
 
     return result if bool(result) else key_pad[row][col]
+
+
+def right(row, col):
+    if col < 4 and is_valid_cell(row, col + 1):
+        col += 1
+    return col
+
+
+
+
--- day_2_puzzle_2/advanced_key_pad_test.py
+++ day_2_puzzle_2/advanced_key_pad_test.py
@@ -17,4 +17,38 @@ class AdvancedKeypadTest(unittest.TestCase):
         self.assertSecurityCode(['U'], '5')
 
     def test_move_when_input_is_out_of_bounds_value(self):
-        self.assertSecurityCode(['D'], '5')
+        self.assertSecurityCode(['DD'], '5')
+
+    def test_move_past_down_max(self):
+        self.assertSecurityCode(['DDDDDDDDD'], '5')
+
+    def test_move_right(self):
+        self.assertSecurityCode(['R'], '6')
+
+    def test_move_max_right(self):
+        self.assertSecurityCode(['RRRR'], '9')
+
+    def test_move_past_max_right(self):
+        self.assertSecurityCode(['RRRRRRRRRR'], '9')
+
+    def test_move_to_first_row_valie(self):
+        self.assertSecurityCode(['RRUU'], '1')
+
+    def test_move_to_first_last_row_value(self):
+        self.assertSecurityCode(['RRDD'], 'D')
+
+    def test_move_to_invalid_left_position(self):
+        self.assertSecurityCode(['RUL'], '2')
+
+    def test_move_to_invalid_right_position(self):
+        self.assertSecurityCode(['RRRDR'], 'C')
+
+    def test_example(self):
+        input_rows = [
+            'ULL',
+            'RRDDD',
+            'LURDL',
+            'UUUUD',
+        ]
+        self.assertSecurityCode(input_rows, '5DB3')
+
