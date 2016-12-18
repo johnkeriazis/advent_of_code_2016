4d9f4df0ff44fe630a02897633c9f86f073b029a
johnkkeriazis <jkeriazis@gmail.com>
2016-12-18 11:25:20 -0500

    support two dimensions

--- day_2_puzzle_1/bathroom_security.py
+++ day_2_puzzle_1/bathroom_security.py
@@ -1,10 +1,9 @@
-from pprint import pformat
-
 key_pad = [
-    ['1','2','3'],
-    ['4','5','6'],
-    ['7','8','9']
+    ['1', '2', '3'],
+    ['4', '5', '6'],
+    ['7', '8', '9']
 ]
+
 
 def security_code_generator(input_rows):
     row = 1
@@ -25,20 +24,10 @@ def security_code_generator(input_rows):
 
     return result
 
+
 input_rows = []
 with open('d0201.txt') as f:
     for line in f.readlines():
         input_rows.append(line)
 
-
 print security_code_generator(input_rows)
-
-##print pformat(input_rows)
-#
-#ctr = 1
-#for row in input_rows:
-#    print ctr
-#    for c in row:
-#        print c
-#    ctr += 1
-#    print '--'
