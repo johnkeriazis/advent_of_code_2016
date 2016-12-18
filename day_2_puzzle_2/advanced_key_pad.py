b0c175aaa69a212cf74bb95695d712c71dbb45f9
johnkkeriazis <jkeriazis@gmail.com>
2016-12-18 16:35:41 -0500

    advanced keypad

--- day_2_puzzle_2/advanced_key_pad.py
+++ day_2_puzzle_2/advanced_key_pad.py
@@ -1,1 +1,33 @@
-__author__ = 'jkeriazis'
+key_pad = [
+    [None, None, '1', None, None],
+    [None, '2',  '3', '4',  None],
+    ['5',  '6',  '7', '8',  '9'],
+    [None, 'A',  'B', 'C',  None],
+    [None, None, 'D', None, None],
+]
+
+
+def up(row, col):
+    if row > 0 and key_pad[row -1][col] is not None:
+        row -= 1
+    return row
+
+
+def advanced_security_code_generator(input_rows):
+    row = 2
+    col = 0
+
+    result = ''
+    for input_row in input_rows:
+        for move in input_row:
+            if move == 'U':
+                row = up(row, col)
+            elif move == 'D' and row < 2:
+                row += 1
+            elif move == 'L' and col > 0:
+                col -= 1
+            elif move == 'R' and col < 2:
+                col += 1
+        result += key_pad[row][col]
+
+    return result if bool(result) else key_pad[row][col]
--- day_2_puzzle_2/advanced_key_pad_test.py
+++ day_2_puzzle_2/advanced_key_pad_test.py
@@ -1,6 +1,20 @@
 import unittest
+from day_2_puzzle_2.advanced_key_pad import advanced_security_code_generator
 
 
 class AdvancedKeypadTest(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def assertSecurityCode(self, input_rows, expected):
+        actual = advanced_security_code_generator(input_rows)
+        self.assertEquals(expected, actual)
+
+    def test_move_with_no_inputs(self):
+        self.assertSecurityCode([], '5')
+
+    def test_move_when_input_is_out_of_bounds_coordinate(self):
+        self.assertSecurityCode(['L'], '5')
+
+    def test_move_when_input_is_out_of_bounds_value(self):
+        self.assertSecurityCode(['U'], '5')
+
+    def test_move_when_input_is_out_of_bounds_value(self):
+        self.assertSecurityCode(['D'], '5')
