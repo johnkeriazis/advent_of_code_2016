5187d8418236ef4a52de0836a19cee3964fdb1a8
John Keriazis <jkeriazis@hedgeserv.com>
2016-12-17 23:26:01 -0500

    one dimensional inputs

--- day_2_puzzle_1/bathroom_security.py
+++ day_2_puzzle_1/bathroom_security.py
@@ -1,4 +1,22 @@
 
 
+key_pad = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
 def bathroom_code_generator(inputs):
-    return 5
+    row = 1
+    col = 1
+    for move in inputs:
+        if move == 'U' and row > 0:
+            row -= 1
+        elif move == 'D' and row < 2:
+            row += 1
+        elif move == 'L' and col > 0:
+            col -= 1
+        elif move == 'R' and col < 2:
+            col += 1
+
+    return key_pad[row][col]
